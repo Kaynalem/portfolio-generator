171c315b62420d4dd54c89a187872df04e4419d2
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-05-13 21:49:02 -0500

    add inquirer

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src');
+//...
+// const pageHTML = generatePage(portfolioData);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//    if (err) throw err;
+//    console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
